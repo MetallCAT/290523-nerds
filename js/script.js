75704cf759acd89521620b65d144ee52d111a6f5
MetallCAT <kiskno@mail.ru>
2016-11-18 15:53:54 +0300

    18.11

--- js/script.js
+++ js/script.js
@@ -1,46 +1,36 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="css/normalize.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <title>Nerds</title>
-  </head>
-  <body>
-    <script>
-      var connect = document.querySelector(".write-to-us");
-      var popup = document.querySelector(".modal-window");
-      var close = popup.querySelector(".modal-window-close");
-      var form = popup.querySelector("form");
-      var name = popup.querySelector("[name=name]");      
-      var mail = popup.querySelector("[name=mail]");
-      var letter = popup.querySelector("[name=letter]");
 
-      connect.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.add("modal-window-show");
-        name.focus();
-      });
+<script>
+  var connect = document.querySelector(".write-to-us");
+  var popup = document.querySelector(".modal-window");
+  var close = popup.querySelector(".modal-window-close");
+  var form = popup.querySelector("form");
+  var name = popup.querySelector("[name=user_name]");      
+  var mail = popup.querySelector("[name=mail]");
+  var letter = popup.querySelector("[name=letter]");
 
-      close.addEventListener("click", function(event) {
-        event.preventDefault();
+  connect.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-window-show");
+    name.focus();
+  });
+
+  close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-window-show");
+  });
+
+  form.addEventListener("submit", function(event) {
+    if (!user_name.value || !mail.value) {  
+      event.preventDefault();
+      console.log("Нужно ввести имя и адресс почты");
+    }   
+  });     
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("modal-window-show")) {
         popup.classList.remove("modal-window-show");
-      });
-
-      form.addEventListener("submit", function(event) {
-        if (!name.value || !male.value) {  
-          event.preventDefault();
-          /*console.log("Нужно ввести имя и адресс почты");*/  
-      });     
-       
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) {
-          if (popup.classList.contains("modal-window-show")) {
-            popup.classList.remove("modal-window-show");
-          }
-        }
-      });
-    </script>
-  </body>
-</html>
+      }
+    }
+  });
+</script>
