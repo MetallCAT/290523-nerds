2b7981419c46d5a5e5b0d1770da7464747b5506f
MetallCAT <kiskno@mail.ru>
2016-11-18 17:00:35 +0300

    карта

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-
-<script>
   var connect = document.querySelector(".write-to-us");
   var popup = document.querySelector(".modal-window");
   var close = popup.querySelector(".modal-window-close");
@@ -33,4 +31,26 @@
       }
     }
   });
-</script>
+
+  var map_Open = document.querySelector(".js-open-map");
+  var map_Popup = document.querySelector(".modal-window-map");
+  var map_Close = map_Popup.querySelector(".modal-window-close");
+
+  map_Open.addEventListener("click", function(event) {
+    event.preventDefault();
+    map_Popup.classList.add("modal-window-show");
+  });
+
+  map_Close.addEventListener("click", function(event) {
+    event.preventDefault();
+    map_Popup.classList.remove("modal-window-show");
+  });
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (map_Popup.classList.contains("modal-window-show")) {
+        map_Popup.classList.remove("modal-window-show");
+      }
+    }
+  });
+
